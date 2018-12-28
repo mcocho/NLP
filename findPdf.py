78eacd701d32e1be10c609b3acecdd2b0444a39d
mcocho <mathieu.cocho@gmail.com>
2018-12-28 16:57:15 +0800

    Update findPdf.py

--- findPdf.py
+++ findPdf.py
@@ -7,7 +7,12 @@ from bs4 import BeautifulSoup
 url = 'https://www.police.wa.gov.au/Traffic/Cameras/Camera-locations'
 page = urllib.request.urlopen(url,timeout=10)
 
-# parse html code and find links to pdf files
+# parse html code and store html elements with links to pdf's in resSet
 soup = BeautifulSoup(page,'html.parser')
-linkTable = soup.find_all(href=re.compile(".pdf"))
+resSet = soup.find_all(href=re.compile(".pdf"))
+
+# extract link strings from resSet and store in list
+linkTable = list()
+for link in resSet:
+    linkTable.append(link.attrs.get('href'))
     
